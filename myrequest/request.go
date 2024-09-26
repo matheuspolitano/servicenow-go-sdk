0a017788b67f867fb4dba2f924c322a4e8de7c40
Matheus Politano <matheuspolitano1@gmail.com>
2024-09-26 16:52:05 +0200

    update request

--- myrequest/request.go
+++ myrequest/request.go
@@ -1,8 +1,10 @@
 package myrequest
 
 import (
+	"bytes"
 	"context"
-	"errors"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"time"
@@ -13,59 +15,72 @@ type HTTPClient struct {
 	client *http.Client
 	baseURL string
 	headers map[string]string
+	username string
+	password string
 }
 
 
 // NewHTTPClient creates and return a new HTTPClient with custom settings
-func NewHTTPClient(baseURL string, timeout time.Duration, headers map[string]string) *HTTPClient{
+func NewHTTPClient(baseURL string, timeout time.Duration, headers map[string]string, username string, password string) *HTTPClient{
 	return &HTTPClient{
 		client: &http.Client{
 			Timeout: timeout,
 		},
 		baseURL: baseURL,
 		headers: headers,
+		username:  username,
+		password: password,
 	}
 }
 
 // Get performs a GET request to the specified endpoint with context for timeout control
-func (h *HTTPClient) Get(ctx context.Context, endpoint string) (*http.Response, error){
+func (h *HTTPClient) Get(ctx context.Context, endpoint string) (map[string]any, error){
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, h.baseURL + endpoint, nil)
 	if err != nil{
 		return nil, err
 	}
+	return h.makeRequest(req)
+}
 
+func (h *HTTPClient) makeRequest(req *http.Request)(map[string]any, error){
 	h.setHeaders(req)
 
+	if h.username != "" && h.password != ""{
+		req.SetBasicAuth(h.username, h.password)
+	}
 	resp, err := h.client.Do(req)
 	if err != nil{
 		return nil, err
 	}
-
-	if resp.StatusCode != http.StatusOK{
-		return nil, errors.New("unexpected status code:" + resp.Status)
+	defer resp.Body.Close()
+	if resp.StatusCode >= http.StatusBadRequest{
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
-	return resp, nil
+	
+	const maxBodySize = 10 * 1024 * 1024
+	bbody, err := io.ReadAll(io.LimitReader(resp.Body, maxBodySize))
+	if err != nil{
+		return nil, err
+	}
+	var body map[string]any
+	if err = json.Unmarshal(bbody, &body); err != nil{
+		return nil, err
+	}
+	
+	return body, nil
 }
 
 // Post performs a POST request to the specified endpoint and body with context for timeout control
-func (h *HTTPClient) Post(ctx context.Context, endpoint string, body io.Reader) (*http.Response, error){
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, h.baseURL + endpoint, body)
+func (h *HTTPClient) Post(ctx context.Context, endpoint string, body any) (map[string]any, error){
+	bodyRequest, err := json.Marshal(body)
 	if err != nil{
 		return nil, err
 	}
-
-	h.setHeaders(req)
-
-	resp, err := h.client.Do(req)
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, h.baseURL + endpoint, bytes.NewReader(bodyRequest))
 	if err != nil{
 		return nil, err
 	}
-
-	if resp.StatusCode != http.StatusCreated{
-		return nil, errors.New("unexpected status code: " + resp.Status) 
-	}
-
-	return resp, nil
+	return h.makeRequest(req)
 }
 
 
